c05424d8841ac1c3039ecf53fbe679ed1e041083
Alexander mokhov <wannasludge@yandex.ru>
2021-09-13 20:06:25 +0300

    Code and gitignore

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 999999;
+        int miles = 20;
+        int totalMiles = price / miles;
+        System.out.println(totalMiles);
+    }
+}
